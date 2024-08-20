3e64d9d7aceb0a4d54deadc58d1e74642fc3881c
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2024-08-21 01:32:13 +0530

    github actions change build db properties fix

--- src/main/java/com/iit/bombay/courses/course_listing/CourseListingApplication.java
+++ src/main/java/com/iit/bombay/courses/course_listing/CourseListingApplication.java
@@ -9,11 +9,5 @@ public class CourseListingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseListingApplication.class, args);
 	}
-//	echo "# course-listing-v1" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main/crypto
-//	git remote add origin https://github.com/AnkushAtCryptoSkool/course-listing-v1.git
-//	git push -u origin main/crypto
+
 }
